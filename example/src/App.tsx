7b87221401043c943b6720e0b43ba5b1bf7e2d2b
HeimerAkousist <heimer_chung@akousist.com>
2024-12-11 16:36:40 +0800

    feat: remove example component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-
 import { Button } from 'akousist-ui';
-import { ExampleComponent } from 'akousist-ui';
 import 'akousist-ui/dist/index.css';
 
 const App = () => {
   return (
     <>
       <Button>Button</Button>
-      <ExampleComponent text="Create React Library Example 😄" />
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,2 @@
-import * as React from 'react';
-import styles from './styles.module.css';
 import './index.scss';
-interface Props {
-  text: string;
-}
-
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>;
-};
-
 export * from './components/button';
